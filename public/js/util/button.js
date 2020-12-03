370d7dc03c8144e1a3aa477358d9b0ee2d6f8abe
Anirudh Manoj <anirudhmanojcv@gmail.com>
2020-12-03 15:53:47 +0530

    bug fix

--- public/js/util/button.js
+++ public/js/util/button.js
@@ -1,5 +1,8 @@
 export function init() {
   document.querySelectorAll("button").forEach((b) => {
+    if (b.classList.contains("mdc-button")) {
+      return;
+    }
     if (!b.classList.contains("no-mdc")) {
       b.classList.add("mdc-button");
       b.classList.add("mdc-button--outlined");
@@ -9,6 +12,7 @@ export function init() {
       b.innerHTML += `
       <span class='mdc-button__label'>${l}</span>
       `;
+      new mdc.ripple.MDCRipple(b);
     }
   });
 }
